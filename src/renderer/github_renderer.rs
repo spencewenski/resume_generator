7cf9fe5cf1c4066e85afbb298d59cdf5aa451318
Spencer Ferris <git@spencewenski.com>
2021-06-13 01:22:25 -0700

    Add github renderer

--- src/renderer/github_renderer.rs
+++ src/renderer/github_renderer.rs
@@ -1,7 +1,78 @@
-pub struct GitHubRenderer;
+use crate::renderer::Renderer;
+use crate::data::{Resume, PersonalInfo, OtherPersonalInfo, Objective, OtherExperience, ProjectInfo, Technologies};
+use std::path::PathBuf;
+use crate::config::Config;
+use crate::util::{write_string_to_file, add_https_to_url, split_string_across_lines};
+use crate::renderer::markdown_renderer::MarkdownRenderer;
+
+pub struct GitHubRenderer {
+    md: MarkdownRenderer,
+}
 
 impl GitHubRenderer {
-    fn new() -> GitHubRenderer {
-        GitHubRenderer
+    pub fn new() -> GitHubRenderer {
+        GitHubRenderer {
+            md: MarkdownRenderer::new(),
+        }
+    }
+}
+
+impl Renderer<Resume, PathBuf> for GitHubRenderer {
+    fn render(self: &Self, element: &Resume, config: &Config) -> Result<PathBuf, String> {
+        let s: String = self.render(element, config)?;
+
+        write_string_to_file(&s,
+                             config.args.output_dir.as_ref(),
+                             &format!("{}-github", &config.args.output_name),
+                             Some(String::from("md")).as_ref())
+    }
+}
+
+impl Renderer<Resume, String> for GitHubRenderer {
+    fn render(self: &Self, element: &Resume, config: &Config) -> Result<String, String> {
+        let mut text = format!("# {}", element.name);
+        text = format!("{}\n\n{}", text, self.render(&element.objective, config)?);
+        text = format!("{}\n\n{}", text, self.render(&element.personal_info, config)?);
+        if let Some(e) = &element.other_experience {
+            text = format!("{}\n\n{}", text, self.render(e, config)?);
+        }
+        if let Some(e) = &element.technologies {
+            text = format!("{}\n\n{}", text, self.render(e, config)?);
+        }
+        Ok(text)
+    }
+}
+
+impl Renderer<PersonalInfo, String> for GitHubRenderer {
+    fn render(self: &Self, element: &PersonalInfo, config: &Config) -> Result<String, String> {
+        let info = self.md.render(element, config)?;
+        // Don't want to display email on GitHub
+        // todo: is there a way to do this without using 'to_owned'?
+        let info = info.split("\n")
+            .map(|x| { x.to_owned()})
+            .filter(|x| {
+                !x.contains(&element.email)
+            })
+            .collect::<Vec<String>>()
+            .join("\n");
+        Ok(info)
+    }
+}
+
+impl Renderer<Objective, String> for GitHubRenderer {
+    fn render(self: &Self, element: &Objective, config: &Config) -> Result<String, String> {
+        self.md.render(element, config)
+    }
+}
+
+impl Renderer<OtherExperience, String> for GitHubRenderer {
+    fn render(self: &Self, element: &OtherExperience, config: &Config) -> Result<String, String> {
+        self.md.render(element, config)
+    }
+}
+
+impl Renderer<Technologies, String> for GitHubRenderer {
+    fn render(self: &Self, element: &Technologies, config: &Config) -> Result<String, String> {
+        self.md.render(element, config)
     }
 }
--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -4,6 +4,7 @@ use crate::config::Config;
 use crate::renderer::pdf_renderer::PdfRenderer;
 use std::path::PathBuf;
 use crate::renderer::markdown_renderer::MarkdownRenderer;
+use crate::renderer::github_renderer::GitHubRenderer;
 
 pub mod github_renderer;
 pub mod markdown_renderer;
@@ -17,7 +18,8 @@ trait Renderer<I, O> {
 pub fn render_resume(resume: &Resume, config: &Config) -> Result<(), String> {
     let renderers : Vec<Box<dyn Renderer<Resume, PathBuf>>> = vec![Box::new(TextRenderer::new()),
                                                                    Box::new(PdfRenderer::new()),
-                                                                   Box::new(MarkdownRenderer::new())];
+                                                                   Box::new(MarkdownRenderer::new()),
+                                                                   Box::new(GitHubRenderer::new())];
     renderers.iter().try_for_each(|x| -> Result<(), String> {
         x.render(resume, config)?;
         Ok(())
