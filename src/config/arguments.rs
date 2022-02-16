519c48b97f42ab62697690666e73b66e48cdc051
Spencer Ferris <git@spencewenski.com>
2022-02-15 18:44:58 -0800

    Use 'clap' instead of 'argparse' for argument parsing.
    'clap' seems to be more popular and has a "derive" api which makes it
    super easy to use.

--- src/config/arguments.rs
+++ src/config/arguments.rs
@@ -1,60 +1,30 @@
-use argparse::{ArgumentParser, Store, StoreOption};
+use clap::Parser;
 
-#[derive(Debug, Serialize, Deserialize, Default)]
+/// Resume Generator.
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
 pub struct Arguments {
+    /// The resume data file.
+    #[clap(short = 'i', long)]
     pub resume_input: String,
+
+    /// The name to use for the generated resumes.
+    #[clap(short, long)]
     pub output_name: String,
+
+    /// The name to use for the generated cover letters. Defaults to appending '-cover_letter' to the output-name.
+    #[clap(short, long)]
     pub cover_letter_output_name: Option<String>,
+
+    /// The directory in which resume files will be placed. The directory is created if it doesn't exist Default: current directory.
+    #[clap(short = 'd', long)]
     pub output_dir: Option<String>,
+
+    /// Configuration file for the various resume formats. Optional.
+    #[clap(short, long)]
     pub format_config: Option<String>,
+
+    /// Override the email in the resume config with this one.
+    #[clap(short, long)]
     pub email: Option<String>,
-}
-
-impl Arguments {
-    pub fn parse_args() -> Arguments {
-        let mut args: Arguments = Default::default();
-        {
-            let mut ap = ArgumentParser::new();
-            ap.set_description("Resume Generator.");
-
-            ap.refer(&mut args.resume_input)
-                .add_option(&["-i", "--resume-input"], Store, "The resume data file.")
-                .required();
-
-            ap.refer(&mut args.output_name)
-                .add_option(
-                    &["-o", "--output-name"],
-                    Store,
-                    "The name to use for the generated resumes.",
-                )
-                .required();
-
-            ap.refer(&mut args.cover_letter_output_name).add_option(
-                &["-c", "--cover-letter-output-name"],
-                StoreOption,
-                "The name to use for the generated cover letters. Defaults to appending '-cover_letter' to the output-name",
-            );
-
-            ap.refer(&mut args.output_dir)
-                .add_option(&["-d", "--output-dir"],
-                            StoreOption,
-                            "The directory in which resume files will be placed. The directory is created if it doesn't exist Default: current directory");
-
-            ap.refer(&mut args.format_config).add_option(
-                &["-f", "--format-config"],
-                StoreOption,
-                "Configuration file for the various resume formats. Optional.",
-            );
-
-            ap.refer(&mut args.email).add_option(
-                &["-e", "--email"],
-                StoreOption,
-                "Override the email in the resume config with this one.",
-            );
-
-            ap.parse_args_or_exit();
-        }
-
-        args
-    }
 }
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,10 +1,11 @@
 use crate::config::arguments::Arguments;
 use crate::config::format_config::FormatConfig;
+use clap::Parser;
 
 pub mod arguments;
 pub mod format_config;
 
-#[derive(Debug, Serialize, Deserialize, Default)]
+#[derive(Debug)]
 pub struct Config {
     pub format_config: FormatConfig,
     pub args: Arguments,
@@ -12,7 +13,7 @@ pub struct Config {
 
 impl Config {
     pub fn new_and_parse_args() -> Result<Config, String> {
-        let args = Arguments::parse_args();
+        let args = Arguments::parse();
 
         let format_config = if let Some(file_name) = &args.format_config {
             FormatConfig::read_from_config_file(file_name)?
